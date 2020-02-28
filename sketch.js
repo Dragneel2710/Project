cba9fb1a6541b77c74df45641bc1566a8adeb73f
Dragneel2710 <61239842+Dragneel2710@users.noreply.github.com>
2020-02-28 20:11:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,22 +1,35 @@
-var fixedRect, movingRect;
+var R1;
+var R2;
+var R;
+var B;
 
 function setup() {
   createCanvas(1200,800);
-  fixedRect = createSprite(400, 100, 50, 80);
-  fixedRect.shapeColor = "green";
-  fixedRect.debug = true;
-  movingRect = createSprite(400, 800,80,30);
-  movingRect.shapeColor = "green";
-  movingRect.debug = true;
-
-  movingRect.velocityY = -5;
-  fixedRect.velocityY = +5;
+  R1 = createSprite(200, 200, 50, 80);
+  R2 = createSprite(400,200,80,30);
+  R = "red";
+  B = "blue";
 }
 
 function draw() {
   background(0,0,0);  
 
- bounce();
- 
+ R2.x = World.mouseX;
+ R2.y = World.mouseY;
+
+ console.log(R1.width/2 + R2.width/2);
+
+ if (R2.x - R1.x < R1.width/2 + R2.width/2 && R1.x - R2.x < R2.width/2 + R1.width/2 && R2.y - R1.y < R1.height/2 + R2.height/2 && R1.y - R2.y < R2.height/2 + R1.height/2 ){
+   R1.shapeColor = "Blue";
+   R2.shapeColor = "Red";
+   background(World.mouseX,World.mouseY,255);
+ }
+
+ else {
+   R1.shapeColor = "Grey";
+   R2.shapeColor = "Grey";
+   background(World.mouseY,0,World.mouseX);
+ }
+
   drawSprites();
 }
