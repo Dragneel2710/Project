6131c5206c765cb2494ad433d989221598c79d6e
Dragneel2710 <61239842+Dragneel2710@users.noreply.github.com>
2020-03-16 18:27:30 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,20 +1,33 @@
-var earth, sun ;
-
-function preload() {
-sun = LoadImage("sun.png");
-earth =  LoadImage("earth.png")
-}
+const World = Matter.World;
+const Engine = Matter.Engine;
+const Bodies = Matter.Bodies;
+var engine;
+var world;
+var body;
+var ground
 
 function setup() {
   createCanvas(400,400);
-  sun = createSprites(200,200,20,20);
-  sun.scale = sun.scale + 1;
-  earth = createSprites(300,200,20,20);
-  earth.scale = 0.5;
+ engine = Engine.create();
+ world = engine.world;
+ var option2 = {
+   restitution : 1, density : 10
+ }
+ body = Bodies.rectangle(200,200,20,20,option2);
+ var options = {
+    isStatic : true
+ }
+ ground = Bodies.rectangle(200,380,300,1,options);
+ World.add(world,body);
+ World.add(world,ground);
+ console.log(body.position.x);
 }
 
 function draw() {
-  background(0,0,0); 
-  sun.scale = sun.scale + 1;
-  drawSprites();
+  background(255,0,0);  
+ Engine.update(engine);
+  rectMode(CENTER);
+  rect(body.position.x,body.position.y,50,50);
+  rect(ground.position.x,ground.position.y,500,10)
+
 }
