3603f1830140f984a8b307ba28188017ed17b0b7
Dragneel2710 <61239842+Dragneel2710@users.noreply.github.com>
2020-03-16 20:14:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,30 +1,39 @@
-var boxes = [];
-var Slider;
- 
-function setup() {
-    createCanvas(400, 400);
-    Slider = createSlider(0, 100, 50);
-    Slider.position(40, 365);
-    Slider.input = map(engine.world.gravity, Slider.min, Slider.max, 0, 1);
-}
- 
-function mousePressed() {
-    if (mouseY < 350) {
-    }
-}
- 
-function draw() {
-    background(51);
-    var fVal = Slider.value();
-}
- 
-function Box(x, y, w, h, options) {
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
 
-   
-    var options = {
+var engine, world;
+var box,box2,box3,box4,box5,box6;
+var pig,pig2;
+var log,log2;
 
-    }
- 
-    this.show = function () {
-    }
-  }
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+    box = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    box3 = new Ground(600,390,1200,20);
+    pig = new Pig(810,350);
+    log = new Log(810,260,300,PI/2)
+    box4 = new Box(700,240,70,70);
+    box5 = new Box(920,240,70,70);
+    //box6 = new Ground(600,390,1200,20);
+    pig2 = new Pig(810,220);
+    log2 = new Log(810,180,300,PI/2)
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box.display();
+    box2.display();
+    box3.display();
+    pig.display();
+    log.display();
+    box4.display();
+    box5.display();
+    //box6.display();
+    pig2.display();
+    log2.display();
+}
